954b6a6c3059046ceab65d3a7a1312667c6cbb3e
Tanishq Patil <trpatil@Mac.lan1>
2026-01-13 00:04:33 -0800

    adding required changes

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+pip install torch==2.4.0 torchvision==0.19.0 --index-url https://download.pytorch.org/whl/cu124
+pip install -r requirements.txt
+pip install gsplat
+cd submodules/gsplat/examples
+pip install -r requirements.txt
+git clone https://github.com/rmbrualla/pycolmap.git
+cd pycolmap
+vim pyproject.toml
+mv pycolmap/ pycolmap2/
+python3 -m pip install -e .
+
